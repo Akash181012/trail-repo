3caffc522e54e9d66f3398ebcf9270f822618cf7
a13213281 <deepak.gupta@africa.airtel.com>
2025-08-31 11:00:18 +0530

    Add Deployment File

---
+++ src/main/java/com/airtel/africa/integration/accountmanagement/externalApiSsl.java
@@ -0,0 +1,8 @@
+package com.airtel.africa.integration.accountmanagement;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import org.apache.camel.support.jsse.KeyStoreParameters;
+import org.apache.camel.support.jsse.SSLContextParameters;
+import org.apache.camel.support.jsse.TrustManagersParameters;
+
+
