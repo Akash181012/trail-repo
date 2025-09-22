67986d75a874c76db21f62363e93b97536071b85
a13213281 <deepak.gupta@africa.airtel.com>
2025-09-22 21:13:23 +0530

    Test External API

--- src/main/java/com/airtel/africa/integration/accountmanagement/route/PartyManagementRoute.java
+++ src/main/java/com/airtel/africa/integration/accountmanagement/route/PartyManagementRoute.java
@@ -104,7 +104,7 @@ public class PartyManagementRoute extends BaseRouteBuilder {
                 .to("https://maps.googleapis.com/maps/api/geocode/json?bridgeEndpoint=true&throwExceptionOnFailure=false"
                         + "&proxyHost=172.27.104.20"
                         + "&proxyPort=4145"
-                        + "&proxyType=http"
+                        + "&proxyType=https"
                         + "&connectTimeout=5000"
                         + "&responseTimeout=10000")
                 .log("Status: ${header.CamelHttpResponseCode}")
