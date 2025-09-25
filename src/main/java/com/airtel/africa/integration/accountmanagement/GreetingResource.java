2ac94259c517b09f7d8fba525623358e7a5ee11b
a13213281 <deepak.gupta@africa.airtel.com>
2025-09-25 08:38:30 +0530

    Test External API

--- src/main/java/com/airtel/africa/integration/accountmanagement/GreetingResource.java
+++ src/main/java/com/airtel/africa/integration/accountmanagement/GreetingResource.java
@@ -24,6 +24,16 @@ import java.security.cert.X509Certificate;
 import java.util.List;
 import java.util.Map;
 
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 @ApplicationScoped
 @Path("/test")
 public class GreetingResource {
@@ -32,6 +42,8 @@ public class GreetingResource {
 
     @Inject
     ProducerTemplate producerTemplate;
+
+
 
     @GET
     @Path("/hello")
@@ -55,4 +67,23 @@ public class GreetingResource {
         }
         return "Hello from Quarkus REST " ;
     }
+
+    @GET
+    @Path("/hello1")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getLocation() throws Exception {
+        String address = "New York";  // example query
+        String url = "https://maps.googleapis.com/maps/api/geocode/json?address=Gurgaon";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI(url))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        return response.body();
+    }
+
 }
